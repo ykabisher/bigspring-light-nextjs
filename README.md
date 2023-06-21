51d00594b99e0fe9a9975b6f54b8a2631b6958d9
Yevgeni Kabisher <yevgeni.kabishcher@gmail.com>
2023-06-21 20:32:36 +0300

    v2 of ds gen

--- README.md
+++ README.md
@@ -104,45 +104,14 @@ npm run dev
 
 After that, it will open up a preview of the template in your default browser, watch for changes to source files, and live-reload the browser when changes are saved.
 
-## Production Build
+## Production Diploy
 
 After finishing all the customization, you can create a production build by running this command.
 
 ```
 npm run build
+cd bigspring-light-nextjs/
+git pull
+yarn
+npm run build
 ```
-
-<!-- reporting issue -->
-## Reporting Issues
-
-We use GitHub Issues as the official bug tracker for this Template. Please Search [existing issues](https://github.com/themefisher/bigspring-light-nextjs/issues). It’s possible someone has already reported the same problem.
-If your problem or idea has not been addressed yet, feel free to [open a new issue](https://github.com/themefisher/bigspring-light-nextjs/issues).
-
-<!-- support -->
-## Technical Support or Questions (Paid)
-
-If you have questions or need help integrating the product please [contact us](https://themefisher.com/contact) instead of opening an issue.
-
-<!-- licence -->
-## License
-
-Copyright (c) 2016 - Present, Designed & Developed by [Themefisher](https://themefisher.com)
-
-**Code License:** Released under the [MIT](https://github.com/themefisher/bigspring-light-nextjs/blob/main/LICENSE) license.
-
-**Image license:** The images are only for demonstration purposes. They have their license, we don't have permission to share those images.
-
-## Hire Us
-
-Besides developing unique, blazing-fast Nextjs templates, we also provide customized services. We specialize in creating affordable, high-quality static websites based on Nextjs.
-
-If you need to customize the theme or complete website development from scratch, you can hire us. **Check Our
-[Contact Page](https://themefisher.com/contact)**
-
-## Nextjs Templates By Us
-
-<a href="https://themefisher.com/products/bookworm-light-nextjs">
-<img src="https://demo.gethugothemes.com/thumbnails/bookworm.png" height="300" width="400"/>
-<p>Bookworm Light Nextjs</p>
-</a>
-
--- pages/ds-mvp.js
+++ pages/ds-mvp.js
@@ -1,7 +1,7 @@
 import config from "@config/config.json";
 import Base from "@layouts/Baseof";
 import "swiper/swiper.min.css";
-import { Button, Container, Form, FormGroup, FormControl, Card, FormLabel, Stack, Spinner } from 'react-bootstrap';
+import { Button, Container, Form, FormGroup, FormControl, Card, FormLabel, Stack, Spinner, Row, Col } from 'react-bootstrap';
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,18 +12,21 @@ const Home = () => {
   const [inputDS, setDS] = useState('all buttons should be green');
   const [inputPrompt, setPrompt] = useState('log in screen');
   const [showSpinner, setShowSpinner] = useState(false);
+  const [exp, setExp] = useState('')
 
   const handleGenerate = async () => {
     setShowSpinner(true);
+    setExp('')
     const response = await fetch('https://ds-gen1-jrsxmikr5q-uc.a.run.app', {
+    // const response = await fetch('http://127.0.0.1:5001/slides-ai-376007/us-central1/ds_gen1', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ ds: inputDS, prompt: inputPrompt })
     });
-    const jsResp = await response.text()
-    console.log(response)
+    const jsResp = await response.json()
+
     const htmlTemplate = `
 <!DOCTYPE html>
 <html lang="en">
@@ -32,14 +35,17 @@ const Home = () => {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+  ${jsResp.style}
+  <body>${jsResp.body}</body>
+
   
-  ${jsResp}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <title>Book Reader App</title>
   </head>
   <body></body>
 </html>
 `
+    setExp(jsResp.explanation)
     setGeneratedHtml(htmlTemplate);
 
 
@@ -48,36 +54,60 @@ const Home = () => {
   };
 
   return (
-    <Base title={title}>
+    <div >
+    
       {/* workflow */}
       <Container>
         <Stack gap={3}>
           <Form>
-            <Stack direction="horizontal" gap={3}>
-              <FormGroup className="me-auto" style={{ width: '100%' }}>
+          <Container>
+      <Row>
+        <Col xs={6}>
+        <FormGroup className="me-auto" style={{ width: '100%' }}>
                 <FormLabel>
                   Design System .md Format
                 </FormLabel>
                 <FormControl
                   as="textarea"
-                  rows={5}
+                  rows={15}
                   value={inputDS}
                   onChange={(e) => setDS(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup className="me-auto" style={{ width: '100%' }}>
-                <FormLabel>
-                  The UI is about
-                </FormLabel>
-                <FormControl
-                  as="textarea"
-                  rows={5}
-                  value={inputPrompt}
-                  onChange={(e) => setPrompt(e.target.value)}
-                />
-              </FormGroup>
-              <Button onClick={handleGenerate}>Generate</Button>
-            </Stack>
+        </Col>
+        <Col xs={6}>
+        <Stack gap={3}>
+                <FormGroup className="me-auto" style={{ width: '100%' }}>
+                  <FormLabel>
+                    The UI is about
+                  </FormLabel>
+                  <FormControl
+                    as="textarea"
+                    rows={5}
+                    value={inputPrompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                  />
+                </FormGroup>
+                <Button onClick={handleGenerate}>Generate</Button>
+                <FormGroup className="me-auto" style={{ width: '100%' }}>
+                  <FormLabel>
+                    Explanation
+                  </FormLabel>
+                  <FormControl
+                    as="textarea"
+                    rows={5}
+                    disabled
+                    value={exp}
+                  />
+                </FormGroup>
+              </Stack>
+        </Col>
+      </Row>
+    </Container>
+          
+            
+              
+        
           </Form>
           <Card>
             <Card.Body>
@@ -95,8 +125,10 @@ const Home = () => {
             </Card.Body>
           </Card>
         </Stack>
+        version 2
       </Container>
-    </Base>
+  
+    </div>
   );
 };
 
